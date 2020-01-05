1d056bb73aecc4b532a36111851dda26f7f8452f
mrhashemian <mr.hashemian2000@gmail.com>
2020-01-05 20:18:56 +0330

    init

--- main.c
+++ main.c
@@ -24,26 +24,38 @@ int main()
 {
     FILE *cinema;
     int hallNumber;
-    scanf("%d", &hallNumber);
-    cinema = fopen("cinema-hall.txt", "w+");
-    struct CinemaHall cinemaHall[hallNumber];
-    printf("Add Halls\n\n");
-    for(int i = 0; i < hallNumber; i++){
-        printf("Hall no.%d:\n", i + 1);
-        printf("Hall Name: ");
-        scanf("%s", cinemaHall[i].hallName);
-        printf("Hall Capacity: ");
-		scanf("%d", &cinemaHall[i].hallCapacity);
-		printf("Schedule Details\n");
-//		cinemaHall.schedule[4] = "hi";
-		printf(" ");
-		printf("\n");
+    char userType;
+    printf("Type (u) if you\'re costumer and (a) if you\'re adminastrator.\n");
+	scanf("%c", &userType);
 		
-		fprintf(cinema, "Hall no.%d:\n", i + 1);
-        fprintf(cinema, "Hall Name: %s\n", cinemaHall[i].hallName);
-        fprintf(cinema, "Hall Capacity: %d\n\n", cinemaHall[i].hallCapacity);
-    }
-
-    fclose(cinema);
+	if(userType == 'a'){
+		scanf("%d", &hallNumber);
+	    cinema = fopen("cinema-hall.txt", "w+");
+	    struct CinemaHall cinemaHall[hallNumber];
+	    printf("Add Halls\n\n");
+	    for(int i = 0; i < hallNumber; i++){
+	        printf("Hall no.%d:\n", i + 1);
+	        printf("Hall Name: ");
+	        scanf("%s", cinemaHall[i].hallName);
+	        printf("Hall Capacity: ");
+			scanf("%d", &cinemaHall[i].hallCapacity);
+			printf("Schedule Details\n");
+	//		cinemaHall.schedule[4] = "hi";
+			printf(" ");
+			printf("\n");
+			
+			fprintf(cinema, "Hall no.%d:\n", i + 1);
+	        fprintf(cinema, "Hall Name: %s\n", cinemaHall[i].hallName);
+	        fprintf(cinema, "Hall Capacity: %d\n\n", cinemaHall[i].hallCapacity);
+	    }
+	
+	    fclose(cinema);
+	}else{
+		cinema = fopen("cinema-hall.txt", "r+");
+		printf("choose Hall\n");
+		char inemaHall;
+		fscanf(cinema,"Hall Name: %c\n", inemaHall);
+		fprintf(cinema, "Hall Name: %s\n", inemaHall);
+	}
     return 0;
 }
