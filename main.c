6258bbabbed6f1f3552ce926bf5fbcbd892138eb
mrhashemian <mr.hashemian2000@gmail.com>
2020-01-28 09:55:40 +0330

    init

--- main.c
+++ main.c
@@ -21,8 +21,49 @@ struct CinemaHall{
     struct CinemaSchedule schedule[];
 } cinemaHall[];
 
-void reserveday(int reserveDate){
-	int i = 0;
+int DayChartoInt(char day){
+	int reserveDayInt;
+	switch(day){
+			case 's':
+			case 'S':
+				reserveDayInt = 0;
+				break;
+			case '1':
+				reserveDayInt = 1;
+				break;
+			case '2':
+				reserveDayInt = 2;
+				break;
+			case '3':
+				reserveDayInt = 3;
+				break;
+			case '4':
+				reserveDayInt = 4;
+				break;
+			case '5':
+				reserveDayInt = 5;
+				break;
+			case 'j':
+			case 'J':
+				reserveDayInt = 6;
+				break;
+		}
+		return(reserveDayInt);
+}
+int reserveday(int reserveDate){
+	int i = 0, reserveStatus = 0;
+	while(cinemaHall[1].schedule[i].date <= reserveDate){
+		if(cinemaHall[1].schedule[i].date == reserveDate){
+			reserveStatus = 1;
+			i++;
+			break;
+		}
+		i++;
+	}
+	i--;
+	if(reserveStatus == 0){
+		return(0);
+	}
 	printf("\nTime\t\tMovie\n");
 	while(cinemaHall[1].schedule[i].date <= reserveDate){
 		if(cinemaHall[1].schedule[i].date == reserveDate){
@@ -30,17 +71,39 @@ void reserveday(int reserveDate){
 		}
 		i++;
 	}
+	return(1);
 }
-void reservetime(int reserveTime, int reserveDate){
+int reservetime(int reserveTime, int reserveDate){
 	int i = 0;
 	while(cinemaHall[1].schedule[i].date <= reserveDate){
 		if(cinemaHall[1].schedule[i].startTime == reserveTime){
 			printf("\"%s\" reserved sucssesfully!\n", cinemaHall[1].schedule[i].movieName);
 			cinemaHall[1].schedule[i].remainingCapacity--;
-//			printf("%d", cinemaHall[1].schedule[i].remainingCapacity);
+			return(1);
 		}
 		i++;
 	}
+	i--;
+	if(cinemaHall[1].schedule[i].startTime != reserveTime){
+		return(0);
+	}
+}
+void checkTime(int day, int start, int end){
+	for(int i = 0; i <= cinemaHall[1].schedule[i].date; ++i){
+		if(i == day){
+			if(start < cinemaHall[1].schedule[i].startTime && end < cinemaHall[1].schedule[i].startTime){
+				not or ok
+			}
+			elseif(start > cinemaHall[1].schedule[i].endTime && end < cinemaHall[1].schedule[i].endTime){
+				false
+			}
+			elseif(start > cinemaHall[1].schedule[i].endTime){
+				need to check with next plan;
+			}
+		}
+		
+	}
+	
 }
 int main()
 {
@@ -87,14 +150,29 @@ int main()
 	fclose(hall);
 	if(userType == 'a'){
 		hall = fopen(hallName, "a");
-		scanf("%d", &cinemaHall[1].schedule[scheduleNums].date);
+		printf("Enter Day: (example:shanbe, 1shanbe, jome)\n");
+		char reserveDay[10];
+		scanf("%s", reserveDay);
+		cinemaHall[1].schedule[scheduleNums].date = DayChartoInt(reserveDay[0]);
+		printf("\nEnter start time:(example: 17)\n");
 		scanf("%d", &cinemaHall[1].schedule[scheduleNums].startTime);
+		printf("\nEnter end time:(example: 19)\n");
 		scanf("%d",	&cinemaHall[1].schedule[scheduleNums].endTime);
+
+
+
+		checkTime(cinemaHall[1].schedule[scheduleNums].date, cinemaHall[1].schedule[scheduleNums].startTime, cinemaHall[1].schedule[scheduleNums].endTime);
+
+
+
+		printf("\nEnter remain capacity:(example: 100)\n");
 		scanf("%d", &cinemaHall[1].schedule[scheduleNums].remainingCapacity);
+		printf("\nEnter movie\'s name:(example: Joker)\n");
 		scanf("%s", cinemaHall[1].schedule[scheduleNums].movieName);
+		printf("schedule added!");
 		fprintf(hall, "\n%d|%d|%d|%d|", cinemaHall[1].schedule[scheduleNums].date, cinemaHall[1].schedule[scheduleNums].startTime, cinemaHall[1].schedule[scheduleNums].endTime, cinemaHall[1].schedule[scheduleNums].remainingCapacity);
 	    fprintf(hall, "%s", cinemaHall[1].schedule[scheduleNums].movieName);
-	    fclose(hall);	
+	    fclose(hall);
 	}else{
 		//choosen hall schual
 		printf("\n%s Plan:\n", cinemaHall[hallNumber - 1].hallName);
@@ -102,31 +180,38 @@ int main()
 		int flag0 = 0, flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0, flag5 = 0, flag6 = 0;
 		for(int i = 0; i < scheduleNums; i++){
 			if(cinemaHall[hallNumber - 1].schedule[i].date == 0 && flag0 == 0){
-				printf("Shanbe\t\t");
+				printf("--------------------------------------------------------------------\n");
+				printf("shanbe\t\t");
 				flag0 = 1;
 			}
 			else if(cinemaHall[hallNumber - 1].schedule[i].date == 1 && flag1 == 0){
-				printf("\n1 Shanbe\t");
+				printf("--------------------------------------------------------------------\n");
+				printf("1shanbe\t\t");
 				flag1 = 1;
 			}
 			else if(cinemaHall[hallNumber - 1].schedule[i].date == 2 && flag2 == 0){
-				printf("\n2 Shanbe\t");
+				printf("--------------------------------------------------------------------\n");
+				printf("2shanbe\t\t");
 				flag2 = 1;
 			}
 			else if(cinemaHall[hallNumber - 1].schedule[i].date == 3 && flag3 == 0){
-				printf("\n3 Shanbe\t");
+				printf("--------------------------------------------------------------------\n");
+				printf("3shanbe\t\t");
 				flag3 = 1;
 			}
 			else if(cinemaHall[hallNumber - 1].schedule[i].date == 4 && flag4 == 0){
-				printf("\n4 Shanbe\t");
+				printf("--------------------------------------------------------------------\n");
+				printf("4shanbe\t\t");
 				flag4 = 1;
 			}
 			else if(cinemaHall[hallNumber - 1].schedule[i].date == 5 && flag5 == 0){
-				printf("\n5 Shanbe\t");
+				printf("--------------------------------------------------------------------\n");
+				printf("5shanbe\t\t");
 				flag5 = 1;
 			}
 			else if(cinemaHall[hallNumber - 1].schedule[i].date == 6 && flag6 == 0){
-				printf("\nJome\t\t");
+				printf("--------------------------------------------------------------------\n");
+				printf("jome\t\t");
 				flag6 = 1;
 			}else{
 				printf("\t\t");
@@ -134,54 +219,31 @@ int main()
 			printf("%d - %d\t\t", cinemaHall[hallNumber - 1].schedule[i].startTime, cinemaHall[hallNumber - 1].schedule[i].endTime);
 			printf("%d\t\t\t", cinemaHall[hallNumber - 1].schedule[i].remainingCapacity);
 			printf("%s\n", cinemaHall[hallNumber - 1].schedule[i].movieName);
-//			printf("%d",i);
 		}
-//		fclose(hall);
 		
 		//Reservation
 		printf("\nChoose Day to Reserve\n");
 		char reserveDay[10];
 		int reserveTime;
-		scanf("%s", reserveDay);
+		int reserveStatus = 0;
 		int reserveDayInt;
-		switch(reserveDay[0]){
-			case 's':
-			case 'S':
-				reserveDayInt = 0;
-				reserveday(reserveDayInt);
-				break;
-			case '1':
-				reserveDayInt = 1;
-				reserveday(reserveDayInt);
-				break;
-			case '2':
-				reserveDayInt = 2;
-				reserveday(reserveDayInt);
-				break;
-			case '3':
-				reserveDayInt = 3;
-				reserveday(reserveDayInt);
-				break;
-			case '4':
-				reserveDayInt = 4;
-				reserveday(reserveDayInt);
-				break;
-			case '5':
-				reserveDayInt = 5;
-				reserveday(reserveDayInt);
-				break;
-			case 'j':
-			case 'J':
-				reserveDayInt = 6;
-				reserveday(reserveDayInt);
-				break;
+		scanf("%s", reserveDay);
+		reserveDayInt = DayChartoInt(reserveDay[0]);
+		reserveStatus = reserveday(reserveDayInt);
+		while(reserveStatus == 0){
+			printf("Choose correct day to reserve:\n");
+			scanf("%s", reserveDay);
+			reserveDayInt = DayChartoInt(reserveDay[0]);
+			reserveStatus = reserveday(reserveDayInt);
 		}
-		printf("\nChoose Time to Reserve\n");
+		reserveStatus = 0;
 		scanf("%d", &reserveTime);
-		reservetime(reserveTime, reserveDayInt);
-		//print movie name and reserve detail
-		//choose to reserve or see movie detail
-		//if detail then to reserve
+		reserveStatus = reservetime(reserveTime, reserveDayInt);
+		while(reserveStatus == 0){
+			printf("Choose correct time to reserve:\n");
+			scanf("%d", &reserveTime);
+			reserveStatus = reservetime(reserveTime, reserveDayInt);
+		}
 		hall = fopen(hallName, "w");
 		for(int i = 0; i < scheduleNums; ++i){
 			fprintf(hall, "%d|%d|%d|%d|", cinemaHall[1].schedule[i].date, cinemaHall[1].schedule[i].startTime, cinemaHall[1].schedule[i].endTime, cinemaHall[1].schedule[i].remainingCapacity);
@@ -189,15 +251,8 @@ int main()
 	        if(i < scheduleNums - 1){
 	        	fprintf(hall, "\n");
 	        }
-//	        fprintf(cinema, "Hall Capacity: %d\n\n", cinemaHall[i].hallCapacity);
 	    }
 	    fclose(hall);
-		
-		
-//		printf("%d", reserveTime);
-		//fseek
-		//fscanf
-	//	fprintf
 	}
     return 0;
 }
